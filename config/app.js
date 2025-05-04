3fdf22f482b238ef498336034e67d8631a69ac80
dcaal-2020531 <dcaal-2020531@kinal.edu.gt>
2025-05-03 23:49:41 -0600

    Se Agrega Login y Se termina User

--- config/app.js
+++ config/app.js
@@ -4,24 +4,26 @@ import express from 'express' //Servidor HTTP
 import morgan from 'morgan' //Logs
 import helmet from 'helmet' //Seguridad para HTTP
 import cors from 'cors' //Acceso al API
-
+import userRoutes from '../src/User/user.routes.js'
+import authRoutes from '../src/auth/auth.routes.js'
 
 const configs = (app)=>{
-    app.use(express.json()) //Aceptar y enviar datos en JSON
-    app.use(express.urlencoded({extended: false})) //No encriptar la URL
+    app.use(express.json()) 
+    app.use(express.urlencoded({extended: false})) 
     app.use(cors())
     app.use(helmet())
     app.use(morgan('dev'))
 }
 
 const routes = (app)=>{
-   // app.use(authRoutes)
+   app.use('/user',userRoutes)
+   app.use('/', authRoutes)
 }
 
 export const initServer = async()=>{
-    const app = express() //Instancia de express
+    const app = express() 
     try{
-        configs(app) //Aplicar configuraciones al servidor
+        configs(app) 
         routes(app)
         app.listen(process.env.PORT)
         console.log(`Server running in port ${process.env.PORT}`)
