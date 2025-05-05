1804e0e93042212c0e5ce08dd61d1a1165011a90
joroxon-2020299 <joroxon-2020299@kinal.edu.gt>
2025-05-04 21:23:33 -0600

    Se arregla app.js para las rutas de los CRUDs

--- config/app.js
+++ config/app.js
@@ -4,8 +4,13 @@ import express from 'express' //Servidor HTTP
 import morgan from 'morgan' //Logs
 import helmet from 'helmet' //Seguridad para HTTP
 import cors from 'cors' //Acceso al API
+
 import productRoutes from '../src/product/product.routes.js';
-import categoryRoutes from '../src/category/category.routes.js'
+import categoryRoutes from '../src/category/category.routes.js';
+
+import inventarioRoutes from '../src/Informes/Inventario/inventario.routes.js'; // Nueva ruta de inventario
+import estadisticasRoutes from '../src//Informes/Estadísticas/estadisticas.routes.js'; // Nueva ruta de estadísticas
+import movimientoInventario from '../src/Informes/movimiento inventario/movimiento.routes.js'; //Movimiento de inventario
 
 
 const configs = (app)=>{
@@ -19,6 +24,10 @@ const configs = (app)=>{
 const routes = (app)=>{
     app.use('/v1/product', productRoutes);
     app.use('/v1/category', categoryRoutes);
+    
+    app.use('/v1/inventario', inventarioRoutes); // Ruta para informes de inventario
+    app.use('/v1/estadisticas', estadisticasRoutes); // Ruta para estadísticas
+    app.use('/v1/movimientoInventario', movimientoInventario); // Movimiento de inventario
 }
 
 export const initServer = async()=>{
